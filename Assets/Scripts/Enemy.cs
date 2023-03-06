8dc2cdb7e34622e25548c391e9b9b92ad98f6647
EmiNiemand <Szymon_PL@live.com>
2023-03-06 13:01:57 +0100

    fix stack overflow for pathfinding

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Palmmedia.ReportGenerator.Core.Logging;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
@@ -147,26 +148,42 @@ public class Enemy : MonoBehaviour
     void SetNewPathWithPathFinding()
     {
         path.Clear();
+
+
+        foreach (var obj in GameObject.FindGameObjectsWithTag("Obstacles"))
+        {
+            var col = obj.GetComponent<BoxCollider>();
+            if (col && ((transform.position.y + bCollider.height / 2 > obj.transform.position.y - col.size.y / 2 &&
+                         transform.position.y - bCollider.height / 2 < obj.transform.position.y - col.size.y / 2) ||
+                        (transform.position.y - bCollider.height / 2 < obj.transform.position.y + col.size.y / 2 && 
+                         transform.position.y + bCollider.height / 2 > obj.transform.position.y + col.size.y / 2) ||
+                        (transform.position.y + bCollider.height / 2 < obj.transform.position.y + col.size.y / 2 &&
+                        transform.position.y - bCollider.height / 2 > obj.transform.position.y - col.size.y / 2)))
+            {
+                if (!hitColliders.ContainsKey(col.GetInstanceID())) hitColliders.Add(col.GetInstanceID(), col);
+            }
+        }
         
-        for (int i = 0; i < 36; i++)
+        // TODO: to delete if ^ this method works
+        /*for (int i = 0; i < 36; i++)
         {
             Vector3 direction = Quaternion.AngleAxis(10f * i, Vector3.up) * Vector3.right;
             Vector3 position = transform.position;
             Vector3 origin = new Vector3(position.x, position.y + bCollider.height / 4, position.z);
             int rayHits = Physics.RaycastNonAlloc(origin, direction, hits, 50f);
-            /*Debug.DrawRay(origin, direction, Color.red, 0.5f);*/
+            /*Debug.DrawRay(origin, direction, Color.red, 0.5f);#1#
             for (int j = 0; j < rayHits; j++)
             {
                 if (!hitColliders.ContainsKey(hits[j].collider.GetInstanceID()) && hits[j].collider.CompareTag("Obstacles")) hitColliders.Add(hits[j].collider.GetInstanceID(), hits[j].collider);
             }
             origin = new Vector3(position.x, position.y - bCollider.height / 4, position.z);
             rayHits = Physics.RaycastNonAlloc(origin, direction, hits, 50f);
-            /*Debug.DrawRay(origin, direction, Color.red, 0.5f);*/
+            /*Debug.DrawRay(origin, direction, Color.red, 0.5f);#1#
             for (int j = 0; j < rayHits; j++)
             {
                 if (!hitColliders.ContainsKey(hits[j].collider.GetInstanceID()) && hits[j].collider.CompareTag("Obstacles")) hitColliders.Add(hits[j].collider.GetInstanceID(), hits[j].collider);
             }
-        }
+        }*/
 
         // Find path using A*
         path = GetComponent<AI.Pathfinding>().FindPath(transform.position, target.transform.position);
