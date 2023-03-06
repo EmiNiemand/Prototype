c36689078f8566087f5aa1c81b36bad347521b68
Adrian_Bronowski <229849@edu.p.lodz.pl>
2023-03-06 18:50:50 +0100

    Added raycast to grappling hook

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class GrapplingHook : MonoBehaviour
 {
     [SerializeField] private GameObject Player;
+    [SerializeField] private float HookRange;
+    private LineRenderer _line;
     
-    // Start is called before the first frame update
     void Start()
     {
-        
+        _line = GetComponent<LineRenderer>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 lineStart = _line.GetPosition(0);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(lineStart, Player.transform.forward, out hit, HookRange))
+            {
+                _line.SetPosition(1, hit.point);
+
+            }
+            else
+            {
+                _line.SetPosition(1, lineStart + (Player.transform.forward * HookRange));
+            }
+        }
     }
 }
